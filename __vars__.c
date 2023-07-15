66fd492e56ad0849907f70d187cebf999aa07228
NullCode1 <mosalah01229@gmail.com>
2023-07-15 00:34:34 +0000

    simple shell

--- __vars__.c
+++ __vars__.c
@@ -39,8 +39,8 @@ int bbc(info_t *info, char *buf, size_t *p)
  * check_chain - checks we should continue chaining based on last status
  * @info: the parameter struct
  * @buf: the char buffer
- * @p: address of current position in buf
- * @i: starting position in buf
+ * @p: Address Of current position in buf
+ * @i: Starting position in buf
  * @len: length of buf
  *
  * Return: Void
